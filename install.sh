7c2113db899421227b2fc2843a4aab52e58d2c11
Timofey Titovets <nefelim4ag@gmail.com>
2016-05-11 02:38:27 +0300

    Update: install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,31 @@
 #!/bin/bash -e
+################################################################################
+# echo wrappers
+INFO(){ echo -n "INFO: "; echo "$@" ;}
+WARN(){ echo -n "WARN: "; echo "$@" ;}
+ERRO(){ echo -n "ERRO: "; echo -n "$@" ; echo " Abort!"; exit 1;}
+
 cd "$(dirname $0)"
 if [ "$UID" != "0" ]; then
-    [ ! -f /usr/bin/sudo ] && echo "Run by root or install sudo!" && exit 1
-    sudo cp -v   ./systemd-swap.sh       /usr/lib/systemd/scripts/systemd-swap.sh
-    sudo cp -v   ./systemd-swap.conf     /etc/
-    sudo ln -svf  /etc/systemd-swap.conf /etc/systemd/swap.conf
-    sudo cp -v   ./systemd-swap.service  /etc/systemd/system/systemd-swap.service
-    sudo cp -v   ./90-systemd-swap.conf  /etc/modprobe.d/90-systemd-swap.conf
+    [ ! -f /usr/bin/sudo ] && ERRO "Run by root or install sudo!" || :
+    SUDO=sudo
 else
-    cp -v   ./systemd-swap.sh       /usr/lib/systemd/scripts/systemd-swap.sh
-    cp -v   ./systemd-swap.conf     /etc/
-    ln -svf  /etc/systemd-swap.conf /etc/systemd/swap.conf
-    cp -v   ./systemd-swap.service  /etc/systemd/system/systemd-swap.service
-    cp -v   ./90-systemd-swap.conf  /etc/modprobe.d/90-systemd-swap.conf
+    unset SUDO
 fi
+
+if [ -f /etc/systemd-swap.conf ]; then
+    INFO "File /etc/systemd-swap.conf already exists"
+    if cmp -s ./systemd-swap.conf /etc/systemd-swap.conf; then
+        :
+    else
+        INFO "New config saved as /etc/systemd-swap.conf.new"
+        $SUDO cp -v ./systemd-swap.conf /etc/systemd-swap.conf.new
+    fi
+else
+    $SUDO cp -v ./systemd-swap.conf /etc/systemd-swap.conf
+fi
+
+$SUDO cp -v   ./systemd-swap.sh       /usr/lib/systemd/scripts/systemd-swap.sh
+$SUDO ln -svf  /etc/systemd-swap.conf /etc/systemd/swap.conf
+$SUDO cp -v   ./systemd-swap.service  /usr/lib/systemd/system/systemd-swap.service
+$SUDO cp -v   ./90-systemd-swap.conf  /etc/modprobe.d/90-systemd-swap.conf
