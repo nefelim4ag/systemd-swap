8725cf8345cb273a7d0b69994c8ab561554fd352
Timofey Titovets <nefelim4ag@gmail.com>
2016-05-12 17:06:05 +0300

    Add package.sh

--- install.sh
+++ install.sh
@@ -5,26 +5,34 @@ INFO(){ echo -n "INFO: "; echo "$@" ;}
 WARN(){ echo -n "WARN: "; echo "$@" ;}
 ERRO(){ echo -n "ERRO: "; echo -n "$@" ; echo " Abort!"; exit 1;}
 
+PREFIX="/"
+case $1 in
+    PREFIX=*) PREFIX=$(echo $1 | cut -d'=' -f2);;
+esac
+
 cd "$(dirname $0)"
-if [ "$UID" != "0" ]; then
-    [ ! -f /usr/bin/sudo ] && ERRO "Run by root or install sudo!" || :
-    SUDO=sudo
-else
-    unset SUDO
+if [ "$PREFIX" == "/" ]; then
+    if [ "$UID" != "0" ]; then
+        [ ! -f /usr/bin/sudo ] && ERRO "Run by root or install sudo!" || :
+        SUDO=sudo
+    else
+        unset SUDO
+    fi
 fi
 
 if [ -f /etc/systemd-swap.conf ]; then
-    INFO "File /etc/systemd-swap.conf already exists"
-    if cmp -s ./systemd-swap.conf /etc/systemd-swap.conf; then
+    INFO "File $PREFIX/etc/systemd-swap.conf already exists"
+    if cmp -s ./systemd-swap.conf $PREFIX/etc/systemd-swap.conf; then
         :
     else
         INFO "New config saved as /etc/systemd-swap.conf.new"
-        $SUDO cp -v ./systemd-swap.conf /etc/systemd-swap.conf.new
+        $SUDO install -Dm 644 ./systemd-swap.conf $PREFIX/etc/systemd-swap.conf.new
     fi
 else
-    $SUDO cp -v ./systemd-swap.conf /etc/systemd-swap.conf
+    $SUDO install -Dm 644 ./systemd-swap.conf $PREFIX/etc/systemd-swap.conf
 fi
 
-$SUDO cp -v   ./systemd-swap.sh       /usr/lib/systemd/scripts/systemd-swap.sh
-$SUDO ln -svf  /etc/systemd-swap.conf /etc/systemd/swap.conf
-$SUDO cp -v   ./systemd-swap.service  /usr/lib/systemd/system/systemd-swap.service
+$SUDO install -Dm755   ./systemd-swap.sh $PREFIX/usr/lib/systemd/scripts/systemd-swap.sh
+$SUDO mkdir -p $PREFIX/etc/systemd/
+$SUDO ln -svf  /etc/systemd-swap.conf $PREFIX/etc/systemd/swap.conf
+$SUDO install -Dm644 ./systemd-swap.service  $PREFIX/usr/lib/systemd/system/systemd-swap.service
