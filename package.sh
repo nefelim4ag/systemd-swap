87f8379f5ef0ecbaf82229ecb0930524d04dbf43
liberodark <liberodark@gmail.com>
2019-06-03 16:34:16 +0200

    Update package.sh

--- package.sh
+++ package.sh
@@ -6,14 +6,14 @@ WARN(){ echo -n "WARN: "; echo "$@" ;}
 ERRO(){ echo -n "ERRO: "; echo -n "$@" ; echo " Abort!"; exit 1;}
 
 debian_package(){
-    cd "$(dirname $0)"
+    cd "$(dirname "$0")"
     VERSION=$(git tag | tail -n 1)
     [ -z "$VERSION" ] && ERRO "Can't get git tag, VERSION are empty!"
     DEB_NAME=systemd-swap_${VERSION}_any
-    mkdir -p $DEB_NAME
-    make install PREFIX=$DEB_NAME/
-    mkdir -p  $DEB_NAME/DEBIAN
-    chmod 755 $DEB_NAME/DEBIAN
+    mkdir -p "$DEB_NAME"
+    make install PREFIX="$DEB_NAME"/
+    mkdir -p  "$DEB_NAME"/DEBIAN
+    chmod 755 "$DEB_NAME"/DEBIAN
     {
         echo "Package: systemd-swap"
         echo "Version: $VERSION"
@@ -25,8 +25,8 @@ debian_package(){
         echo "Installed-Size: 16"
         echo "Maintainer: nefelim4ag@gmail.com"
         echo "Description: Script for creating hybrid swap space from zram swaps, swap files and swap partitions."
-    } > $DEB_NAME/DEBIAN/control
-    dpkg-deb --build $DEB_NAME
+    } > "$DEB_NAME"/DEBIAN/control
+    dpkg-deb --build "$DEB_NAME"
 }
 
 archlinux_package(){
@@ -34,12 +34,12 @@ archlinux_package(){
 }
 
 fedora_package(){
-    cd "$(dirname $0)"
+    cd "$(dirname "$0")"
     FEDORA_VERSION=$1
     VERSION=$(git tag | tail -n 1)
     [ -z "$VERSION" ] && ERRO "Can't get git tag, VERSION are empty!"
     [ -z "$FEDORA_VERSION" ] && ERRO "Please specify fedora version e.g.: $0 fedora f28"
-    fedpkg --release $FEDORA_VERSION local
+    fedpkg --release "$FEDORA_VERSION" local
     mv noarch/*.rpm ./
     rmdir noarch
     rm *.src.rpm
@@ -48,6 +48,6 @@ fedora_package(){
 case $1 in
     debian) debian_package ;;
     archlinux) archlinux_package ;;
-    fedora) fedora_package $2 ;;
+    fedora) fedora_package "$2" ;;
     *) echo "$0 <debian|archlinux|fedora [version]>" ;;
 esac
